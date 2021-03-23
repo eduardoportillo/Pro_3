249608f6f6c34c2c4c1090d33d6a390a4a2cf94a
eduardoportillo <eportillovirreira@gmail.com>
2021-03-23 03:53:17 -0400

    agregando clase frame

--- P1_sort_algorithm/src/main/main.java
+++ P1_sort_algorithm/src/main/main.java
@@ -1,9 +1,14 @@
 package main;
 
-public class main {
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 
+public class Main {
+    
+    private final static Logger log = LogManager.getRootLogger();
+    
     public static void main(String[] args) {
-        
+        log.debug("hola");
     }
     
 }
