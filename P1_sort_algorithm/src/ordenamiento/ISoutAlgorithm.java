fda3c6faa02d531b4629f6131da091238a106f5e
eduardoportillo <eportillovirreira@gmail.com>
2021-03-23 04:08:43 -0400

    todas las clases creadas

---
+++ P1_sort_algorithm/src/ordenamiento/ISoutAlgorithm.java
@@ -0,0 +1,5 @@
+package ordenamiento;
+
+public interface ISoutAlgorithm {
+    
+}
--- P1_sort_algorithm/src/vista/JFrameInit.java
+++ P1_sort_algorithm/src/vista/JFrameInit.java
@@ -5,8 +5,8 @@ import javax.swing.JFrame;
 public class JFrameInit extends JFrame{
     public JFrameInit (){
         this.setSize(620,400);     
-        this.setDefaultCloseOperation(EXIT_ON_CLOSE);       //Para cerrar la ventana en close
-        this.setLocationRelativeTo(null);       //Para habrir el frame al medio
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);       
+        this.setLocationRelativeTo(null);      
         this.setVisible(true);
         this.setTitle("Algoritmos de ordenamiento");
         this.setResizable(false);
